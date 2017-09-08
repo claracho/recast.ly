7afcc7c80244f76ff297153787c8db3378c7b678
Hack Reactor Student <student@hackreactor.com>
2017-09-08 15:25:57 -0700

    create state variable

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,51 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><Search /></div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: exampleVideoData,
+      video: exampleVideoData[0]
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><Search /></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><VideoPlayer video={this.state.video}/></div>
+          </div>
+          <div className="col-md-5">
+            <div><VideoList videos={this.state.videos}/></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><VideoPlayer /></div>
-      </div>
-      <div className="col-md-5">
-        <div><VideoList videos={exampleVideoData}/></div>
-      </div>
-    </div>
-  </div>
-);
+    );    
+  }
+
+}
+
+// var App = () => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><Search /></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <div><VideoPlayer video={exampleVideoData[0]}/></div>
+//       </div>
+//       <div className="col-md-5">
+//         <div><VideoList videos={exampleVideoData}/></div>
+//       </div>
+//     </div>
+//   </div>
+// );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
